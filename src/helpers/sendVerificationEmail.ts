d99657c09fbb1dad449c17844618d11f3863fdea
prathamgarg03 <gargpratham.312003@gmail.com>
2024-06-25 19:28:43 +0530

    create verificaton email functionality

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,3 +1,22 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
 
+export async function sendverificationEmail(
+    email: string,
+    username: string,
+    verificationCode: string
+): Promise<ApiResponse> {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: "Secret Line | Verification Code",
+            react: VerificationEmail({ username, otp: verificationCode }),
+          });
+        return {success: true, message: "Verification email sent successfully"}
+    } catch (emailError) {
+        console.error("Error sending verification email", emailError)
+        return {success: false, message: "Failed to send verification email"}
+    }
+}
