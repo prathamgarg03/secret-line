e1493f203cfb25afb7905045d583814826eb2516
prathamgarg03 <gargpratham.312003@gmail.com>
2024-06-26 01:09:56 +0530

    Create AuthProvider.tsx

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
-function SignInPage() {
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
   return (
-    <div>SignInPage</div>
+    <>
+      Not signed in <br />
+      <button className="bg-orange-500 px-3 py-1 m-4 rounded-lg"onClick={() => signIn()}>Sign in</button>
+    </>
   )
 }
-
-export default SignInPage
