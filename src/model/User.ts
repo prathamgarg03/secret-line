be1a81197d223a9d70f4ad20955e26c71aa4fd04
prathamgarg03 <gargpratham.312003@gmail.com>
2024-06-25 17:54:51 +0530

    Update user model

--- src/model/User.ts
+++ src/model/User.ts
@@ -70,3 +70,5 @@ const UserSchema: Schema<User> = new Schema({
 
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || 
                 (mongoose.model<User>("User", UserSchema))
+
+export default UserModel
