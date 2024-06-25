dac3a474c4e0001866801a7948558a2fcf8dd37d
prathamgarg03 <gargpratham.312003@gmail.com>
2024-06-25 17:54:14 +0530

    Update user model

--- src/model/User.ts
+++ src/model/User.ts
@@ -67,3 +67,6 @@ const UserSchema: Schema<User> = new Schema({
     },
     messages: [MessageSchema]
 })
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || 
+                (mongoose.model<User>("User", UserSchema))
