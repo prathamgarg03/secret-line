6047530a6c113990c710cd0deafafc9db74d4093
prathamgarg03 <gargpratham.312003@gmail.com>
2024-08-05 10:17:02 +0530

    Update layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
       <main className='flex-1 mt-32 sm:mt-16 overflow-hidden'>
       {children}
       </main>
-      <footer className="w-full text-center py-4 bg-gray-100">
+      <footer className="w-full text-center py-4 bg-gray-100 mt-16">
         <p className="text-sm text-gray-500">© 2024 Secret Line. All rights reserved.</p>
       </footer>
     </div>
